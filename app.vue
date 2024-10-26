1be9ffd372a01d0e0b1c7dfef255637a9dd8aa94
ivan-hryshko <giver313@gmail.com>
2024-10-26 22:43:45 +0300

    feat: added list selector

--- app.vue
+++ app.vue
@@ -2,37 +2,68 @@
   <div class="main">
     <div class="container">
       <div
+        v-if="isListSelected"
         class="actions-list"
       >
         <div
-          v-for="action in actionsList"
+          v-for="action in actionsList[selectedListIndex].actions"
           :key="action"
           class="action"
         >
-          {{ action }}
+          {{ action}}
         </div>
       </div>
+      <div
+        v-else
+        class="actions-list"
+      >
+        <div
+          v-for="(list, index) in actionsList"
+          :key="list"
+          class="action"
+          @click="selectList(index)"
+        >
+          {{ list.name }}
+        </div>
+      </div>
+
       <div class="footer">
         <div
+          v-if="isListSelected"
           class="action"
         >
           Обрати активність
         </div>
       </div>
     </div>
-    </div>
+  </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
+const selectedListIndex = ref(null)
 const actionsList = ref([
-  'Вязати',
-  'Гратись з кішкою',
-  'Гратись з кішкою',
-  'Вязати',
-  'Гратись з кішкою',
+  {
+    name: 'today',
+    actions: [
+      'Вязати',
+      'Гратись з кішкою',
+      'Гратись з кішкою',
+      'Вязати',
+      'Гратись з кішкою',
+    ]
+  }
 ])
+
+const isListSelected = computed(() => {
+  return selectedListIndex.value === 0 || selectedListIndex.value > 0
+})
+
+function selectList(index) {
+  console.log('index :>> ', index);
+  selectedListIndex.value = index
+}
 </script>
 
 <style lang="scss">
@@ -52,18 +83,30 @@ const actionsList = ref([
 }
 
 .actions-list {
-  display: grid;
-  grid-template-columns: auto auto;
-  grid-auto-rows: min-content;
+  display: flex;
+  flex-direction: column;
   gap: 8px;
   padding: 8px;
   overflow-y: auto;
 }
+// .actions-list {
+//   display: grid;
+//   grid-template-columns: auto auto;
+//   grid-auto-rows: min-content;
+//   gap: 8px;
+//   padding: 8px;
+//   overflow-y: auto;
+// }
 .action {
   padding: 8px;
   border: 1px solid rgb(180, 178, 178);
   border-radius: 4px;
   background-color: white;
+}
+
+.footer {
+  display: flex;
+  gap: 8px;
 }
 
 @media screen and (max-width: 768px) {
